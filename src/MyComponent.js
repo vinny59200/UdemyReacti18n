fd6ac8f015833641dd2cc8586c1042c25725c69f
vvauban <vvauban@gmail.com>
2022-12-09 09:13:20 +0100

    Init

---
+++ src/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// the hook
+import { useTranslation } from 'react-i18next';
+
+function MyComponent () {
+  const { t, i18n } = useTranslation();
+  return <h1>{t('Welcome to React')}</h1>
+}
+
+export default MyComponent;
